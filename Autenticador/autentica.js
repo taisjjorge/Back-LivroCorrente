e69357588cd13103eca6c39da6bede1f3ef9744d
Atila-Aguiar <atilasilva007@gmail.com>
2021-02-18 14:24:49 -0300

    Adição de rota de Edição

--- Autenticador/autentica.js
+++ Autenticador/autentica.js
@@ -3,11 +3,12 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) =>{
 
     try{
-        const token = req.headers.Authorization.split(' ')[1]
+        const token = req.headers.authorization.split(' ')[1]
         const decodificador = jwt.verify(token, "RCD")
         req.dados = decodificador
         next()
-    } catch {
+    } catch (e) {
+        console.log(e)
         res.json({"Mensagem": "Token Invalido"})
     }
 
